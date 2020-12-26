bda889b5422e7d16363c596a8c774dfcdc190272
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-12-26 16:45:48 -0500

    updated routing for loggedin users and added chat

--- src/app/App.js
+++ src/app/App.js
@@ -4,22 +4,25 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../views/Home";
 import About from "../views/About";
 import Login from "../views/Login";
+import Chat from "../views/Chat";
 import Nav from "../components/Nav";
 
 import CustomThemeProvider from "../components/ThemeContext";
+import UserProvider from "../components/UserContext";
 
 const App = () => {
   return (
-    <CustomThemeProvider>
-      <Router>
+    <UserProvider>
+      <CustomThemeProvider>
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/chats/:id" component={Chat} />
         </Switch>
-      </Router>
-    </CustomThemeProvider>
+      </CustomThemeProvider>
+    </UserProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app";
 
-document.getElementById("root").style.height = "100vh";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
