70054025462617d4f8e73c2b2c83fd7afad12d15
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-12-27 01:39:09 -0500

    dashboard fetche all chats, small tweak on userctx

--- src/components/UserContext/UserContext.js
+++ src/components/UserContext/UserContext.js
@@ -21,53 +21,13 @@ const UserProvider = ({ children }) => {
     const chatRef = db.ref("chats").child(chatID).child("members");
     console.log("validating chat...");
     await chatRef.on("value", (snapshot) => {
-      if (snapshot.exists() && snapshot.val().includes(user.id)) {
+      if (snapshot.exists()) {
         console.log("User has logged in");
       } else {
         history.push("/login");
       }
     });
   }
-
-  async function addUser(name) {
-    const userKey = db.ref("users").push().getKey();
-    const chatRef = db.ref("chats").child("main");
-    const userRef = db.ref("users").child(userKey);
-    const info = {
-      name: name,
-      createdDate: Date.now(),
-      isLoggedIn: true,
-      currentChat: "main",
-      id: userKey,
-    };
-
-    await chatRef.child("members").push(userKey);
-    await userRef.set(info);
-    setUser(info);
-    localStorage.setItem("currentUser", userKey);
-    history.push(`/dashboard`);
-  }
-
-  async function fetchUser(id) {
-    await db
-      .ref("users")
-      .child(id)
-      .once("value", (snapshot) => {
-        if (snapshot.exists()) {
-          setUser(snapshot.val());
-        }
-      });
-
-    await db
-      .ref("users")
-      .child(id)
-      .once("value", (snapshot) => {
-        if (snapshot.exists()) {
-          setUser(snapshot.val());
-        }
-      });
-  }
-
   React.useState(() => {
     const unlisten = auth().onAuthStateChanged((profile) => {
       if (profile) {
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,11 +1,82 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { Link } from "react-router-dom";
+import { UserContext } from "../components/UserContext";
+import { db } from "../services/firebase";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    textAlign: "center",
+    margin: "1rem",
+  },
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
+  const { user } = React.useContext(UserContext);
+  const [chats, setChats] = React.useState(null);
+  const [loading, setLoading] = React.useState();
+
+  async function fetchChat() {
+    setLoading(true);
+    await db.ref("chats").on("value", (snapshot) => {
+      if (snapshot.exists()) {
+        let results = [];
+        snapshot.forEach((snap) => {
+          const MEMBERS = Object.values(snap.val().members);
+          const KEY = snap.key;
+          console.log("MEMBERS", MEMBERS);
+          MEMBERS.forEach((m) => {
+            if (m.id === user.uid) {
+              console.log("This user is in this chat");
+              results.push(KEY);
+            }
+          });
+        });
+        console.log("All Chats", results);
+        setChats(results);
+        setLoading(false);
+      }
+    });
+  }
+
+  React.useEffect(() => {
+    fetchChat();
+  }, []);
+
   return (
     <Container>
-      <Typography variant="h3">Dashboard</Typography>
+      <Typography variant="h3" className={classes.title}>
+        Dashboard
+      </Typography>
+      <Paper>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          spacing={2}
+        >
+          {chats &&
+            chats.map((chat) => {
+              return (
+                <Grid item key={chats} component={Link} to={`/chats/${chat}`}>
+                  <Typography
+                    style={{ textDecoration: "none" }}
+                    color="textPrimary"
+                    variant="h5"
+                  >
+                    {chat}
+                  </Typography>
+                </Grid>
+              );
+            })}
+        </Grid>
+      </Paper>
     </Container>
   );
 };
